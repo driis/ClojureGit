70081d8cef3adeab78170822d311ace93694662f
Dennis Riis <dr@driis.dk>
2013-09-30 22:53:27 +0200

    Very rudimentary unzip of git objects - (figuring out how to do Java interop)

--- src/cgit/util/zip.clj
+++ src/cgit/util/zip.clj
@@ -1,8 +1,14 @@
 (ns cgit.util.zip
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str])
+  (:import (java.io FileInputStream IOException FileNotFoundException)
+           (java.util.zip InflaterInputStream)))
 
 (defn path-from-hash [hash]
-  (apply str (concat ".git/" (take 2 hash) "/" (drop 2 hash))))
+  (apply str (concat ".git/objects/" (take 2 hash) "/" (drop 2 hash))))
 
 (defn unzip-blob [hash]
-  (path-from-hash hash))
+  (let [file (path-from-hash hash)]
+    (let [inflater (java.util.zip.InflaterInputStream. (java.io.FileInputStream. file))
+          buffer (make-array Byte/TYPE 1024)]
+      (.read inflater buffer 0 1024)
+      (apply str (map char buffer)))))
--- test/util/zip_test.clj
+++ test/util/zip_test.clj
@@ -3,4 +3,4 @@
             [cgit.util.zip :refer :all]))
 
 (deftest i-can-get-blob-path
-  (is (= ".git/47/a59d0ad0cc623199741d6f98cf3bc3f0e473b2" (path-from-hash "47a59d0ad0cc623199741d6f98cf3bc3f0e473b2"))))
+  (is (= ".git/objects/47/a59d0ad0cc623199741d6f98cf3bc3f0e473b2" (path-from-hash "47a59d0ad0cc623199741d6f98cf3bc3f0e473b2"))))
