fbc44cfb20322cbfdb09144ff4c263b4bd6877e8
Dennis Riis <dr@driis.dk>
2013-09-30 22:17:24 +0200

    Configuring the REPL

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
 (defproject cgit "0.1.0-SNAPSHOT"
   :description "Git in Clojure"
   :license {:name "MIT License"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :repl-options {
+    ;; Specify the ns to start the REPL in (overrides :main in
+    ;; this case only)
+    :init-ns cgit})
--- src/cgit.clj
+++ src/cgit.clj
@@ -1,4 +1,5 @@
-(ns app.core)
+(ns cgit
+  (:require [cgit.util.zip :refer :all]))
 
 (defn foo
   "I don't do a whole lot."
